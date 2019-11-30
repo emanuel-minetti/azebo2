26b35f80c326d11cd5b735c94e17bb8ac5f43f02
Emanuel Minetti <e.minetti@posteo.de>
2019-11-30 23:41:57 +0100

    Adjusted code file headers.

--- server/config/application.config.php
+++ server/config/application.config.php
@@ -1,10 +1,28 @@
 <?php
+
 /**
- * If you need an environment-specific system or application configuration,
- * there is an example in the documentation
- * @see https://docs.zendframework.com/tutorials/advanced-config/#environment-specific-system-configuration
- * @see https://docs.zendframework.com/tutorials/advanced-config/#environment-specific-application-configuration
+ * azebo2 is an application to print working time tables
+ * Copyright (C) 2019  Emanuel Minetti
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * @author Emanuel Minetti <e.minetti@posteo.de>
+ * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
+ * @copyright Copyright (c) 2019 Emanuel Minetti
+ * @license   https://www.gnu.org/licenses/gpl-3.0.en.html GPLv3
  */
+
 return [
     // Retrieve list of modules used in this application.
     'modules' => require __DIR__ . '/modules.config.php',
