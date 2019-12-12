69923d504176dd89619c9faccfed5cce26fccff1
Emanuel Minetti <e.minetti@posteo.de>
2019-12-12 22:03:36 +0100

    Continued styling.

--- client/src/App.vue
+++ client/src/App.vue
@@ -6,7 +6,10 @@
           <img src="./assets/UdK-Logo_lang.jpg"
                alt="The UdK logo with a link to the homepage of this application"/>
         </router-link>
-        <h1>Arbeitzeitbogen</h1>
+        <hgroup>
+        <h1>Arbeitzeitbogen für</h1>
+          <h1>{{ name }}</h1>
+        </hgroup>
       </div>
       <nav id="nav">
         <router-link to="/">Home</router-link>
@@ -18,6 +21,16 @@
   </div>
 </template>
 
+<script lang="ts">
+import {Component, Vue} from "vue-property-decorator";
+
+@Component
+  export default class App extends Vue {
+  // TODO get the Name from session
+    name = "Ihr Name";
+}
+</script>
+
 <style lang="scss">
 #app {
   font-family: "Avenir", Helvetica, Arial, sans-serif;
@@ -27,12 +40,24 @@
   color: #2c3e50;
 }
 
+#logo {
+  width: 80%;
+  display: inline-block;
+}
+
 #logo img {
   height: 10em;
 }
 
+#logo h1 {
+  display: inline-block;
+  text-align: center;
+}
+
 #nav {
-  padding: 30px;
+  //padding: 30px;
+  width: 20%;
+  display: inline-block;
 
   a {
     font-weight: bold;
