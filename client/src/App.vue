e42eb84628a99e2f6cfd99a10bbfc7d43daf5007
Emanuel Minetti <e.minetti@posteo.de>
2019-12-15 01:14:49 +0100

    Added custom variables file for Bootstrap and set a primary color.

--- client/src/App.vue
+++ client/src/App.vue
@@ -52,6 +52,13 @@
 </script>
 
 <style lang="scss">
+    // Import custom SASS variable overrides
+    @import 'assets/custom-vars.scss';
+
+    // Import Bootstrap and BootstrapVue source SCSS files
+    @import '~bootstrap/scss/bootstrap.scss';
+    @import '~bootstrap-vue/src/index.scss';
+
     #app {
         font-family: "Avenir", Helvetica, Arial, sans-serif;
         -webkit-font-smoothing: antialiased;
