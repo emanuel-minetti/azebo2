b26eeb953317f45945fb888dc9f015d5bcec3945
Emanuel Minetti <e.minetti@posteo.de>
2019-12-18 00:55:46 +0100

    Added a client side guard and a (fake) login page.

---
+++ client/src/views/Login.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>Das ist die Login-Seite</div>
+</template>
+
+<script lang="ts">
+
+    import {Component, Vue} from "vue-property-decorator";
+
+    @Component
+    export default class Login extends Vue {
+
+    }
+</script>
+
+<style scoped>
+
+</style>
