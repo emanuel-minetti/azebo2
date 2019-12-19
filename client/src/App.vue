e4fa261c161024073d1fe59d19e3bfb276001c3c
Emanuel Minetti <e.minetti@posteo.de>
2019-12-19 22:59:50 +0100

    Adjusted editor settings to eslint/prettier rules and continued to implement a form on the login page.

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,117 +1,115 @@
 <template>
-    <div id="app">
-        <header>
-            <div id="logo">
-                <router-link to="/">
-                    <img
-                            src="./assets/UdK-Logo_lang.jpg"
-                            alt="Das Logo der UdK"
-                    />
-                </router-link>
-                <hgroup>
-                    <h1>Arbeitszeitbogen für</h1>
-                    <h1>{{ name }}</h1>
-                </hgroup>
-            </div>
-            <nav id="nav">
-                <ul>
-                    <li>
-                        <button class="btn btn-primary">
-                            <router-link to="/">Einstellungen</router-link>
-                        </button>
-                    </li>
-                    <li>
-                        <button class="btn btn-primary">
-                            <router-link to="/about">Monate</router-link>
-                        </button>
-                    </li>
-                    <li>
-                        <button class="btn btn-primary">
-                            <router-link to="/about">Logout</router-link>
-                        </button>
-                    </li>
-                </ul>
-            </nav>
-        </header>
-        <router-view/>
-        <footer>
-            &copy; {{ copyrightyear }} Emanuel Minetti, UdK Berlin Version: {{ version }}
-        </footer>
-    </div>
+  <div id="app">
+    <header>
+      <div id="logo">
+        <router-link to="/">
+          <img src="./assets/UdK-Logo_lang.jpg" alt="Das Logo der UdK" />
+        </router-link>
+        <hgroup>
+          <h1>Arbeitszeitbogen für</h1>
+          <h1>{{ name }}</h1>
+        </hgroup>
+      </div>
+      <nav id="nav">
+        <ul>
+          <li>
+            <button class="btn btn-primary">
+              <router-link to="/">Einstellungen</router-link>
+            </button>
+          </li>
+          <li>
+            <button class="btn btn-primary">
+              <router-link to="/about">Monate</router-link>
+            </button>
+          </li>
+          <li>
+            <button class="btn btn-primary">
+              <router-link to="/about">Logout</router-link>
+            </button>
+          </li>
+        </ul>
+      </nav>
+    </header>
+    <router-view />
+    <footer>
+      &copy; {{ copyrightyear }} Emanuel Minetti, UdK Berlin Version:
+      {{ version }}
+    </footer>
+  </div>
 </template>
 
 <script lang="ts">
-    import {Component, Vue} from "vue-property-decorator";
+import { Component, Vue } from "vue-property-decorator";
 
-    @Component
-    export default class App extends Vue {
-        // TODO get the Name from session
-        name = "Ihr Name";
-        // TODO get copyright and version from `package.json`
-        copyrightyear = "2019";
-        version = "0.1.0";
-    }
+@Component
+export default class App extends Vue {
+  // TODO get the Name from session
+  name = "Ihr Name";
+  // TODO get copyright and version from `package.json`
+  copyrightyear = "2019";
+  version = "0.1.0";
+}
 </script>
 
 <style lang="scss">
-    // Import custom SASS variable overrides
-    @import 'assets/custom-vars.scss';
+// Import custom SASS variable overrides
+@import "assets/custom-vars.scss";
 
-    // Import Bootstrap and BootstrapVue source SCSS files
-    @import '~bootstrap/scss/bootstrap.scss';
-    @import '~bootstrap-vue/src/index.scss';
+// Import Bootstrap and BootstrapVue source SCSS files
+@import "~bootstrap/scss/bootstrap.scss";
+@import "~bootstrap-vue/src/index.scss";
 
-    #app {
-        font-family: "Avenir", Helvetica, Arial, sans-serif;
-        -webkit-font-smoothing: antialiased;
-        -moz-osx-font-smoothing: grayscale;
-        //text-align: center;
-        color: #2c3e50;
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  //text-align: center;
+  color: #2c3e50;
+}
+
+#logo {
+  width: 60%;
+  display: inline-block;
+
+  img {
+    height: 10em;
+  }
+
+  hgroup {
+    display: inline-block;
+    padding-left: 100px;
+    text-align: center;
+  }
+}
+
+#nav {
+  width: 40%;
+  display: inline-block;
+
+  ul {
+    list-style-type: none;
+
+    li {
+      padding: 0 20px;
+      display: inline;
     }
+  }
 
-    #logo {
-        width: 60%;
-        display: inline-block;
+  a {
+    font-weight: bold;
+    color: white;
+  }
 
-        img {
-            height: 10em;
-        }
+  a:hover {
+    text-decoration-line: none;
+  }
 
-        hgroup {
-            display: inline-block;
-            padding-left: 100px;
-            text-align: center;
-        }
-    }
+  button:hover {
+    background-color: theme-color("text");
+  }
+}
 
-    #nav {
-        width: 40%;
-        display: inline-block;
-
-        ul {
-            list-style-type: none;
-
-            li {
-                padding: 0 20px;
-                display: inline;
-            }
-        }
-
-        a {
-            font-weight: bold;
-            color: white;
-        }
-
-        a:hover {
-            text-decoration-line: none;
-        }
-
-        button:hover {
-            background-color: theme-color("text");
-        }
-    }
-
-    footer {
-        text-align: center;
-    }
+footer {
+  text-align: center;
+}
 </style>
--- client/src/views/Login.vue
+++ client/src/views/Login.vue
@@ -1,39 +1,49 @@
 <template>
-    <div>
-        <Title prop-title="Login" />
-        <b-form @submit="onSubmit">
-            <b-form-group
-                    label="Benutzername:"
-                    label-for="username">
-                <b-form-input v-model="form.username" placeholder="Ihr Benutzername">
-                </b-form-input>
-            </b-form-group>
-        </b-form>
-    </div>
+  <div>
+    <Title prop-title="Login" />
+    <b-form @submit="onSubmit">
+      <b-form-group label="Benutzername:" label-for="username">
+        <b-form-input
+          id="username"
+          v-model="form.username"
+          placeholder="Ihr Benutzername"
+        >
+        </b-form-input>
+      </b-form-group>
+      <b-form-group label="Passwort:" label-for="password">
+        <b-form-input
+          id="password"
+          v-model="form.password"
+          placeholder="Ihr Passwort"
+        >
+        </b-form-input>
+      </b-form-group>
+    </b-form>
+  </div>
 </template>
 
 <script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import Title from "@/components/Title.vue";
 
-    import {Component, Vue} from "vue-property-decorator";
-    import Title from "@/components/Title.vue";
+@Component({
+  components: { Title }
+})
+export default class Login extends Vue {
+  form = {
+    username: "",
+    password: ""
+  };
 
-    @Component({
-        components: {Title}
-    })
-    export default class Login extends Vue {
-        form = {
-            username: '',
-            password: ''
-        };
-
-        onSubmit(evt: Event) {
-            evt.preventDefault();
-        }
-    }
+  onSubmit(evt: Event) {
+    evt.preventDefault();
+  }
+}
 </script>
 
 <style scoped>
-    form {
-        width: 80%;
-    }
+form {
+  padding-left: 10%;
+  padding-right: 10%;
+}
 </style>
