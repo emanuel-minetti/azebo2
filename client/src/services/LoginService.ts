02ce950dd9c4f0ffb086afc0cd95eb6469456ed6
Emanuel Minetti <e.minetti@posteo.de>
2019-12-22 02:49:54 +0100

    Login is working.
    Redirecting and guarding not yet implemented.

--- client/src/services/LoginService.ts
+++ client/src/services/LoginService.ts
@@ -1,7 +1,5 @@
 export default class LoginService {
   static login(username: String, password: String) {
-    // eslint-disable-next-line no-console
-    console.log(JSON.stringify({ username, password }));
     const requestData = JSON.stringify({
       username,
       password
@@ -10,18 +8,37 @@ export default class LoginService {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
-        "Accept": "application/json",
+        Accept: "application/json"
       },
       body: requestData
     };
     //TODO make configurable
     return fetch("http://localhost:7000/api/login", requestOptions)
-      .then(answer => {
-        // eslint-disable-next-line no-console
-        console.log(answer);
-      })
-      .catch(answer => {
-        // eslint-disable-next-line no-console
-        console.log(answer);
+      .then(this.handleResponse)
+      .then(user => {
+        if (user.jwt) {
+          localStorage.setItem("user", JSON.stringify(user));
+        }
+        return user;
       });
-};}
+  }
+
+  private static handleResponse(response: Response) {
+    return response.text().then(text => {
+      const data = text && JSON.parse(text);
+      if (!response.ok) {
+        if (response.status == 401) {
+          LoginService.logout();
+          location.reload(true);
+        }
+        const error = (data && data.message) || response.statusText;
+        return Promise.reject(error);
+      }
+      return data.data;
+    });
+  }
+
+  private static logout() {
+    localStorage.removeItem("user");
+  }
+}
