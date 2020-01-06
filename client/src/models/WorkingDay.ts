929bbfb2beffaefeee8336a374ee9fea6b63044d
Emanuel Minetti <e.minetti@posteo.de>
2020-01-06 23:45:29 +0100

    Encapsulated `WorkingDay`.

--- client/src/models/WorkingDay.ts
+++ client/src/models/WorkingDay.ts
@@ -1,29 +1,128 @@
 export default class WorkingDay {
-  date: Date;
-  begin?: Date;
-  end?: Date;
-  timeOff?: string;
-  comment?: string;
-  break: boolean;
-  afternoon: boolean;
-  afternoonBeginn?: Date;
-  afternoonEnd?: Date;
+  private _date: Date;
+  private _begin?: Date;
+  private _end?: Date;
+  private _timeOff?: string;
+  private _comment?: string;
+  private _break: boolean;
+  private _afternoon: boolean;
+  private _afternoonBeginn?: Date;
+  private _afternoonEnd?: Date;
+
+  private _edited: boolean;
 
   constructor(data?: any) {
     if (data && data.date && data.break && data.afternoon) {
-      this.date = new Date(data.date);
-      this.break = data.break;
-      this.afternoon = data.afternoon;
-      this.begin = data.begin;
-      this.end = data.ent;
-      this.timeOff = data.time_off;
-      this.comment = data.comment;
-      this.afternoonBeginn = data.afternoon_beginn;
-      this.afternoonEnd = data.afternoon_end;
+      this._date = new Date(data.date);
+      this._break = data.break;
+      this._afternoon = data.afternoon;
+      this._begin = data.begin;
+      this._end = data.end;
+      this._timeOff = data.time_off;
+      this._comment = data.comment;
+      this._afternoonBeginn = data.afternoon_beginn;
+      this._afternoonEnd = data.afternoon_end;
     } else {
-      this.date = new Date();
-      this.break = false;
-      this.afternoon = false;
+      this._date = new Date();
+      this._break = false;
+      this._afternoon = false;
+    }
+    this._edited = false;
+  }
+
+  get date(): Date {
+    return this._date;
+  }
+
+  // no setter for `date`
+
+  get begin(): Date | undefined {
+    return this._begin;
+  }
+
+  set begin(value: Date | undefined) {
+    if (value) {
+      this._begin = value;
+      this._edited = true;
     }
   }
+
+  get end(): Date | undefined {
+    return this._end;
+  }
+
+  set end(value: Date | undefined) {
+    if (value) {
+      this._end = value;
+      this._edited = true;
+    }
+  }
+
+  get timeOff(): string | undefined {
+    return this._timeOff;
+  }
+
+  set timeOff(value: string | undefined) {
+    if (value) {
+      this._timeOff = value;
+      this._edited = true;
+    }
+  }
+
+  get comment(): string | undefined {
+    return this._comment;
+  }
+
+  set comment(value: string | undefined) {
+    if (value) {
+      this._comment = value;
+      this._edited = true;
+    }
+  }
+
+  get break(): boolean {
+    return this._break;
+  }
+
+  set break(value: boolean) {
+    this._break = value;
+    this._edited = true;
+  }
+
+  get afternoon(): boolean {
+    return this._afternoon;
+  }
+
+  set afternoon(value: boolean) {
+    this._afternoon = value;
+    this._edited = true;
+  }
+
+  get afternoonBeginn(): Date | undefined {
+    return this._afternoonBeginn;
+  }
+
+  set afternoonBeginn(value: Date | undefined) {
+    if (value) {
+      this._afternoonBeginn = value;
+      this._edited = true;
+    }
+  }
+
+  get afternoonEnd(): Date | undefined {
+    return this._afternoonEnd;
+  }
+
+  set afternoonEnd(value: Date | undefined) {
+    if (value) {
+      this._afternoonEnd = value;
+      this._edited = true;
+    }
+  }
+
+  get edited(): boolean {
+    return this._edited;
+  }
+
+  // no setter for `date`
 }
