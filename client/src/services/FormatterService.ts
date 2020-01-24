c18242beb8af44be7ddaa5c78a2b39992343cc78
Emanuel Minetti <e.minetti@posteo.de>
2020-01-24 14:43:03 +0100

    Reviewed the formatter service.

--- client/src/services/FormatterService.ts
+++ client/src/services/FormatterService.ts
@@ -1,4 +1,8 @@
 export default class FormatterService {
+  /**
+   * Converts a `Date` to german localized (DD.MM.YYYY) string.
+   * @param date the date to convert
+   */
   public static toGermanDate(date: Date) {
     const options = {
       year: "numeric",
@@ -8,12 +12,20 @@ export default class FormatterService {
     return date.toLocaleString("de-DE", options);
   }
 
+  /**
+   * Converts a `Date` to german localized (Wochentag, DD.MM.YYYY) string.
+   * @param date the date to convert
+   */
   public static toLongGermanDate(date: Date) {
     const options = { weekday: "long" };
     const weekday = date.toLocaleString("de-DE", options);
     return weekday + ", " + FormatterService.toGermanDate(date);
   }
 
+  /**
+   * Converts a `Date` to german localized (hh:mm) string.
+   * @param date the date to convert
+   */
   public static toGermanTime(date?: Date) {
     if (date) {
       const options = { hour: "numeric", minute: "numeric" };
