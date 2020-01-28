7a9e2c568765efa5c6c6dc51976202702d5c7477
Emanuel Minetti <e.minetti@posteo.de>
2020-01-28 12:56:59 +0100

    Updated ddl.sql file.

--- server/assets/sql/create_working_day.sql
+++ server/assets/sql/create_working_day.sql
@@ -1,22 +1,20 @@
 drop table  if exists working_day;
 create table working_day
 (
-    id int auto_increment,
-    user_id int not null,
-    date date not null,
-    begin time null,
-    end time null,
-    time_off varchar(20) null,
-    comment text null,
-    break boolean default false not null,
-    afternoon boolean default false not null,
-    afternoon_begin time null,
-    afternoon_end time null,
-    constraint working_day_pk
-        primary key (id),
+    id              int auto_increment
+        primary key,
+    user_id         int                  not null,
+    date            date                 not null,
+    begin           time                 null,
+    end             time                 null,
+    time_off        varchar(20)          null,
+    comment         text                 null,
+    break           tinyint(1) default 0 not null,
+    afternoon       tinyint(1) default 0 not null,
+    afternoon_begin time                 null,
+    afternoon_end   time                 null,
+    constraint working_day_user_id_date_uindex
+        unique (user_id, date),
     constraint working_day_user_id_fk
         foreign key (user_id) references user (id)
 );
-
-create unique index working_day_user_id_date_uindex
-    on working_day (user_id, date);
