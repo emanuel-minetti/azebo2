2ada74d271f3cf6825b9a656ffe0d1771bb4a655
Emanuel Minetti <e.minetti@posteo.de>
2020-02-08 01:42:50 +0100

    azebo2-15: Started repairing 404. See #18.

--- client/src/views/NotFound.vue
+++ client/src/views/NotFound.vue
@@ -3,3 +3,11 @@
     <h1>Sorry.The requested resource was not found on this server.</h1>
   </div>
 </template>
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component
+export default class NotFound extends Vue {
+
+}
+</script>
