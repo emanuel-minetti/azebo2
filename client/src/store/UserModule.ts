c26dcd0f486f0dc85f6fbcd40209210a93697922
Emanuel Minetti <e.minetti@posteo.de>
2020-02-11 18:48:14 +0100

    azebo2-21 Removed debugging.

--- client/src/store/UserModule.ts
+++ client/src/store/UserModule.ts
@@ -17,11 +17,7 @@ const UserModule: Module<any, any> = {
       return LoginService.login(credentials.username, credentials.password)
         .then(data => {
           commit("setUser", new User(data.user));
-          //TODO remove debugging
-          console.log("Nanu");
-          //return data;
         })
-        //.catch(() => commit("setUser", new User()));
     }
   }
 };
