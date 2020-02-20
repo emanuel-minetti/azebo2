184354d511d97a932202cf4a43c7559449ce8e19
Emanuel Minetti <e.minetti@posteo.de>
2020-02-20 15:43:02 +0100

    azebo2-24 Introduced component `DayForm`.

--- client/src/components/DayForm.vue
+++ client/src/components/DayForm.vue
@@ -1,14 +1,44 @@
 <template>
-  <div>
-    Hallo!
-  </div>
+  <b-form @submit="onSubmit" @reset="onReset" v-if="show">
+    <b-form-group label="Arbeitsbeginn:" label-for="begin-input">
+      <b-form-input
+        id="begin-input"
+        v-model="form.begin"
+        required
+        placeholder="Arbeitsbeginn"
+      ></b-form-input>
+    </b-form-group>
+    <b-button type="submit" variant="primary">Submit</b-button>
+    <b-button type="reset" variant="danger">Reset</b-button>
+  </b-form>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 
 @Component
-export default class DayForm extends Vue {}
+export default class DayForm extends Vue {
+  form = {
+    begin: ""
+  };
+  show = true;
+
+  onSubmit(evt: Event) {
+    evt.preventDefault();
+    console.log(JSON.stringify(this.form));
+  }
+
+  onReset(evt: Event) {
+    evt.preventDefault();
+    // Reset our form values
+    this.form.begin = "";
+    // Trick to reset/clear native browser form validation state
+    this.show = false;
+    this.$nextTick(() => {
+      this.show = true;
+    });
+  }
+}
 </script>
 
 <style scoped></style>
