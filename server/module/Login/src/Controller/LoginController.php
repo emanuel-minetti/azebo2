68b0d99a8d83d2ca3b13271bd3a79d4226d04369
Emanuel Minetti <e.minetti@posteo.de>
2020-02-27 15:48:23 +0100

    azebo2-19 Started implementing LDAP authentication.

--- server/module/Login/src/Controller/LoginController.php
+++ server/module/Login/src/Controller/LoginController.php
@@ -1,20 +1,19 @@
 <?php
 /**
-* azebo2 is an application to print working time tables
-*
-* @author Emanuel Minetti < e . minetti@posteo . de >
-* @link      https://github.com/emanuel-minetti/azebo2
-* @copyright Copyright(c) 2019 - 2020 Emanuel Minetti
-* @license   https://www.gnu.org/licenses/gpl-3.0.en.html GPLv3
-*/
+ * azebo2 is an application to print working time tables
+ *
+ * @author Emanuel Minetti < e . minetti@posteo . de >
+ * @link      https://github.com/emanuel-minetti/azebo2
+ * @copyright Copyright(c) 2019 - 2020 Emanuel Minetti
+ * @license   https://www.gnu.org/licenses/gpl-3.0.en.html GPLv3
+ */
 
 
 namespace Login\Controller;
 
-use RuntimeException;
+use Laminas\Config\Factory;
 use Laminas\Mvc\Controller\AbstractActionController;
 use Laminas\View\Model\JsonModel;
-
 use Login\Model\UserTable;
 use Service\AuthorizationService;
 
@@ -50,12 +49,41 @@ class LoginController extends AbstractActionController
             return $declineRequest;
         }
 
-        // get user from DB
-        try {
-            $user = $this->table->getUserByUsername($username);
-        } catch (RuntimeException $e) {
-            return $declineRequest;
+//        // get user from DB
+//        try {
+//            $user = $this->table->getUserByUsername($username);
+//        } catch (RuntimeException $e) {
+//            return $declineRequest;
+//        }
+
+        $config = Factory::fromFile('./../server/config/ldap.config.php', true);
+        $options = $config->ldap->toArray();
+        $baseDn = $options['baseDn'];
+        $host = $options['host'];
+        $internBaseDn = "ou=intra,$baseDn";
+        $externBaseDn = "ou=people,$baseDn";
+        $internDn = "uid=$username,$internBaseDn";
+        $externDn = "uid=$username,$externBaseDn";
+        exec("ldapsearch -h $host -D '$internDn' -w $password -Z -b '$internDn'", $ldif, $val);
+        if ($val !== 0) {
+            exec("ldapsearch -h $host -D '$externDn' -w $password -Z -b '$externDn'", $ldif, $val);
         }
+        if ($val === 0) {
+            foreach ($ldif as $line) {
+                if (substr($line, 0, 4) === "sn: ") {
+                    $test['nachname'] = substr($line, 4);
+                }
+                if (substr($line, 0, 11) === 'givenName: ') {
+                    $test['vorname'] = substr($line, 11);
+                }
+            }
+        } else {
+            $test = "Nicht authentifiziert";
+
+        }
+        var_dump($test);
+        var_dump($val);
+        die();
 
         // authenticate
         if ($user->verifyPassword($password)) {
