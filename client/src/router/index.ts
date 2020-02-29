acce4d0d4e3bff849f63eabd6eaced1f808d9e03
Emanuel Minetti <e.minetti@posteo.de>
2020-02-29 22:52:30 +0100

    azebo2-29 Repaired lazy loading 'About'.

--- client/src/router/index.ts
+++ client/src/router/index.ts
@@ -28,8 +28,8 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    // @ts-ignore
-    component: () => import(/* webpackChunkName: "about"  */"@/views/About")
+    component: () =>
+      import(/* webpackChunkName: "about"  */ "../views/About.vue")
   },
   {
     // route all other request to a 404 page
--- client/src/views/index.ts
+++ client/src/views/index.ts
@@ -1,6 +1,5 @@
-import About from "./About.vue";
 import Home from "./Home.vue";
 import Login from "./Login.vue";
 import NotFound from "./NotFound.vue";
 
-export { About, Home, Login, NotFound };
+export { Home, Login, NotFound };
