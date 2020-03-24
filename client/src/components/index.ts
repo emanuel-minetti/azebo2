6b884efa7b1301db57e9c7f3a527c67d9a2be891
Emanuel Minetti <e.minetti@posteo.de>
2020-03-24 14:11:16 +0100

    azebo2-29 In Code maintenance.

--- client/src/components/index.ts
+++ client/src/components/index.ts
@@ -1,10 +1,9 @@
 import Title from "./Title.vue";
 import MonthTable from "./MonthTable.vue";
 import MonthAggregate from "./MonthAggregate.vue";
-import DayForm from "./DayForm.vue";
 import RulesForm from "./RulesForm.vue";
 import CarryForm from "./CarryForm.vue";
 
 // Dont export nested components here, because they are not found
 // with nested exports
-export { Title, MonthTable, MonthAggregate, DayForm, RulesForm, CarryForm };
+export { Title, MonthTable, MonthAggregate, RulesForm, CarryForm };
