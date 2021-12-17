290e3a8075f621bc6c73bc6273a734302063dd59
Emanuel Minetti <e.minetti@posteo.de>
2021-12-17 15:19:43 +0100

    Completed validating input for `setDayAction()`.

--- server/validation/TimeOffValidator.php
+++ server/validation/TimeOffValidator.php
@@ -15,11 +15,29 @@ class TimeOffValidator extends AbstractValidator
     ];
 
     /**
-     * Validates whether `$value` is in list.
+     * Validates whether `$value` is in the list of time-offs. If is it and its value forbids 'begin' and 'end',
+     * it validates that theese are not set.
+     *
+     * @param array $value an array containing `before`, `end` and `timeOff`.
      */
     public function isValid($value): bool
     {
         $timeOffs = Factory::fromFile('./../server/config/timeOffs.config.php');
-        return in_array($value, $timeOffs);
+        $timeOff = $value['timeOff'];
+        if (!in_array($timeOff, $timeOffs)) return false;
+        if ($timeOff == "urlaub" ||
+            $timeOff == "azv" ||
+            $timeOff == "gruen" ||
+            $timeOff == "krank" ||
+            $timeOff == "kind" ||
+            $timeOff == "reise" ||
+            $timeOff == "befr" ||
+            $timeOff == "sonder" ||
+            $timeOff == "bildung_url" ||
+            $timeOff == "bildung") {
+                if (isset($value['begin'])) return false;
+                if (isset($value['end'])) return false;
+        }
+        return true;
     }
 }
