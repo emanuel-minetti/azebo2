89bf21f7a789daf822731c4d391cf19cdaca5197
Emanuel Minetti <e.minetti@posteo.de>
2023-01-06 21:00:08 +0100

    Added some static content to the working rules view.

--- client/src/views/WorkingRules.vue
+++ client/src/views/WorkingRules.vue
@@ -5,7 +5,18 @@
       Hier können Sie Ihre Arbeitszeitregelungen einsehen und neue Regelungen hinzufügen.
     </div>
     <p>
-      Das ist ein Probetext.
+      Eine Arbeitszeitregeung kann nach ihrer Erstellung weder verändert noch gelöscht werden.<br>
+    </p>
+    <p>
+      Sollte sich Ihre Arbeitszeitregelung ändern (z.B. wegen einer Vertragsänderung oder weil Sie an anderen
+      Wochentagen arbeiten),<br> erstellen Sie bitte eine neue Regelung mit dem entsprechenden Anfangsdatum.
+      (Das Enddatum der bestehenden Regelung wird dabei automatisch angepasst.)
+    </p>
+    <p>
+      Sollten Sie eine Regelung irrtümlich erstellt haben erstellen Sie eine neue Regelung mit <b>demselben</b>
+      Anfagsdatum, wie die zu ersetzende.<br> Diese überschreibt dann die alte Regelung. Wenn Sie dies tun, ändern sich
+      ihre Soll-Arbeitszeiten für den gesamten Gültigkeitszeitraum und es erscheint ein Vermerk auf Ihrem nächsten
+      Ausdruck.
     </p>
     <b-table
     bordered
