a123f75ab8bbaa6061705261c26aff904e03a28d
Emanuel Minetti <e.minetti@posteo.de>
2023-02-03 15:37:26 +0100

    Styled the message a bit.

--- server/module/Message/src/Model/Message.php
+++ server/module/Message/src/Model/Message.php
@@ -6,16 +6,19 @@ use Laminas\Config\Factory;
 use ReturnTypeWillChange;
 
 class Message {
+    public string $title;
     public string $text;
     public string $variant;
     public function __construct() {
         $config = Factory::fromFile('./../server/config/message.config.php', true);
+        $this->title = $config->get('title');
         $this->text = $config->get('text');
         $this->variant = $config->get('variant');
     }
 
     #[ReturnTypeWillChange] public function getArrayCopy(): array {
         return [
+            'title' => $this->title,
             'text' => $this->text,
             'variant' => $this->variant,
         ];
