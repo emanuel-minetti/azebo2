a2202e7ac4cb656630ea44d1df0f4be06dca929f
Emanuel Minetti <e.minetti@posteo.de>
2023-02-15 13:57:20 +0100

    Code maintenance and prepared directory for pdfs.

--- server/module/Print/src/Controller/PrintController.php
+++ server/module/Print/src/Controller/PrintController.php
@@ -1,14 +1,16 @@
-<?php
+<?php /** @noinspection PhpUnused */
 
 namespace Print\Controller;
 
+use AzeboLib\ApiController;
+use Laminas\View\Model\JsonModel;
 use Service\log\AzeboLog;
 
-class PrintController extends \AzeboLib\ApiController {
+class PrintController extends ApiController {
     public function __construct(AzeboLog $logger) {
         parent::__construct($logger);
     }
-    public function printAction() {
+    public function printAction(): JsonModel {
         $this->prepare();
         $result = [
             'text' => "Hallo from Print",
